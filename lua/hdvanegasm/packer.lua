33378e63b98399003778be6e964ecc260f103dc5
hernan232 <hdvanegasm@gmail.com>
2023-03-26 09:21:15 -0500

    Add color scheme

--- lua/hdvanegasm/packer.lua
+++ lua/hdvanegasm/packer.lua
@@ -43,4 +43,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use('Mofiqul/dracula.nvim')
 end)
